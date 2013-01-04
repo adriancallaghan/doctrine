2597973e988d59aa5b66748a14736879b0abc13a
Adrian <adrian@Fedora.laptop>
2013-01-04 15:59:15 +0000

    Successfull installation of doctrine

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -6,11 +6,17 @@ class IndexController extends Zend_Controller_Action
     public function init()
     {
         /* Initialize action controller here */
+        $registry = Zend_Registry::getInstance();
+        $this->_em = $registry->entitymanager;
     }
 
     public function indexAction()
     {
         // action body
+        $testEntity = new Default_Model_User;
+        $testEntity->setName('Hector Pinol');
+        $this->_em->persist($testEntity);
+        $this->_em->flush();
     }
 
 
